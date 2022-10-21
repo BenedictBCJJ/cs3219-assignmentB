40e0928c8fc3ecec68af4278b8662bcfda40e317
BenedictBCJJ <bennyyxz@gmail.com>
2022-10-22 04:39:44 +0800

    Add contacts api from tutorial

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let apiRoutes = require("./routes/api-routes");
+// Import Body parser
+let bodyParser = require("body-parser"); // Import Mongoose
+let mongoose = require("mongoose"); // Configure bodyparser to handle post requests
 
-var app = express();
+// Initialize the app
+let app = express(); // Setup server port
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Import routes
+// Configure bodyparser to handle post requests
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+// Connect to Mongoose and set connection variable
+mongoose.connect("mongodb://localhost/resthub", { useNewUrlParser: true });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Heroku Mongoose connection
+// mongoose.connect('mongodb://heroku_5686p02g:sia8l3fni4jmu7qbn0ac1t75mf@ds349857.mlab.com:49857/heroku_5686p02g', { useNewUrlParser: true });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var db = mongoose.connection;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Added check for DB connection
+
+if (!db) console.log("Error connecting db");
+else console.log("Db connected successfully");
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get("/", (req, res) => res.send("Hello World with Express"));
+
+// Use Api routes in the App
+app.use("/api", apiRoutes);
+// Launch app to listen to specified port
+app.listen(port, function () {
+  console.log("Running RestHub on port " + port);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
