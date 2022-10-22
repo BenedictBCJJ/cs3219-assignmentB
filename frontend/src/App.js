c69afe757702e6b485c1644fdeb7b7c0d7b63cfb
BenedictBCJJ <bennyyxz@gmail.com>
2022-10-22 18:51:58 +0800

    Add post and update to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Quotes from "./GetAll.js";
 import GetSingular from "./GetOne";
+import Post from "./Post";
+import Update from "./Update";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Quotes />
         <GetSingular />
+        <Post />
+        <Update />
       </header>
     </div>
   );
---
+++ frontend/src/Post.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+
+import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import qs from "qs";
+function processData(id, name, gender, email, phone) {
+  return { id, name, gender, email, phone };
+}
+
+function Post() {
+  const [rows, setRows] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [gender, setGender] = useState("");
+
+  function post() {
+    const data = {
+      name: name,
+      email: email,
+      phone: phone,
+      gender: gender,
+    };
+    const options = {
+      method: "POST",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      data: data,
+      url: "http://localhost:5000/api/contacts",
+    };
+    axios.post("http://localhost:5000/api/contacts", data).then((response) => {
+      console.log(response);
+      let tempRow = [];
+
+      let payload = response.data["data"];
+      tempRow.push(
+        processData(
+          payload._id,
+          payload.name,
+          payload.gender,
+          payload.email,
+          payload.phone
+        )
+      );
+
+      setRows(tempRow);
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={post}>Create Entry</button>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "15ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="filled-basic"
+          label="Type Name here"
+          variant="filled"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          id="filled-basic"
+          label="Type Email here"
+          variant="filled"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          id="filled-basic"
+          label="Type Phone here"
+          variant="filled"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <TextField
+          id="filled-basic"
+          label="Type Gender here"
+          variant="filled"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        />
+      </Box>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Gender</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Phone</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.gender}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.phone}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
+export default Post;
