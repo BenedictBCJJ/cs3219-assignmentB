827bdb2a221530ad21ed37e20ff4f0b6dd4baba8
BenedictBCJJ <bennyyxz@gmail.com>
2022-11-12 10:31:17 +0800

    no message

--- routes/contactController.js
+++ routes/contactController.js
@@ -7,12 +7,13 @@ exports.index = function (req, res) {
         status: "error",
         message: err,
       });
+    } else {
+      res.json({
+        status: "success",
+        message: "Contacts retrieved successfully",
+        data: contacts,
+      });
     }
-    res.json({
-      status: "success",
-      message: "Contacts retrieved successfully",
-      data: contacts,
-    });
   });
 }; // Handle create contact actions
 exports.new = function (req, res) {
@@ -22,36 +23,45 @@ exports.new = function (req, res) {
   contact.email = req.body.email;
   contact.phone = req.body.phone; // save the contact and check for errors
   contact.save(function (err) {
-    if (err) res.json(err);
-    res.json({
-      message: "New contact created!",
-      data: contact,
-    });
+    if (err) {
+      res.json(err);
+    } else {
+      res.json({
+        message: "New contact created!",
+        data: contact,
+      });
+    }
   });
 }; // Handle view contact info
 exports.view = function (req, res) {
   Contact.findById(req.params.contact_id, function (err, contact) {
-    if (err) res.send(err);
-    res.json({
-      message: "Contact details loading..",
-      data: contact,
-    });
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        message: "Contact details loading..",
+        data: contact,
+      });
+    }
   });
 }; // Handle update contact info
 exports.update = function (req, res) {
   Contact.findById(req.params.contact_id, function (err, contact) {
-    if (err) res.send(err);
-    contact.name = req.body.name ? req.body.name : contact.name;
-    contact.gender = req.body.gender;
-    contact.email = req.body.email;
-    contact.phone = req.body.phone; // save the contact and check for errors
-    contact.save(function (err) {
-      if (err) res.json(err);
-      res.json({
-        message: "Contact Info updated",
-        data: contact,
+    if (err) {
+      res.send(err);
+    } else {
+      contact.name = req.body.name ? req.body.name : contact.name;
+      contact.gender = req.body.gender;
+      contact.email = req.body.email;
+      contact.phone = req.body.phone; // save the contact and check for errors
+      contact.save(function (err) {
+        if (err) res.json(err);
+        res.json({
+          message: "Contact Info updated",
+          data: contact,
+        });
       });
-    });
+    }
   });
 }; // Handle delete contact
 exports.delete = function (req, res) {
@@ -60,11 +70,14 @@ exports.delete = function (req, res) {
       _id: req.params.contact_id,
     },
     function (err, contact) {
-      if (err) res.send(err);
-      res.json({
-        status: "success",
-        message: "Contact deleted",
-      });
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({
+          status: "success",
+          message: "Contact deleted",
+        });
+      }
     }
   );
   //test
